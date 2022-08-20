0a05708b7dc0db7f828d98685f133dc538c7a062
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2022-08-20 19:57:23 -0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Core;
+using static Core.SentimentAnalysis;
+
+Console.WriteLine("Hello, World!");
+
+ModelInput modelInput = new ModelInput
+{
+    LoggedIn = true,
+    SentimentText = "this is a great day. congratulations"
+};
+
+var salida = SentimentAnalysis.Predict(modelInput);
+
+
+ModelInput modelInput2 = new ModelInput
+{
+    LoggedIn = true,
+    SentimentText = "you are rude"
+};
+
+var salida2 = SentimentAnalysis.Predict(modelInput2);
+Console.WriteLine(salida);
+Console.ReadKey();
