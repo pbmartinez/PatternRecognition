d12bcd7912739934a220a9cc9c2bad82749dc71d
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2022-08-21 19:42:38 -0400

    Add blazor page for image analysis

--- WebApi/Controllers/PatternRecognitionController.cs
+++ WebApi/Controllers/PatternRecognitionController.cs
@@ -1,5 +1,8 @@
 ﻿using Core;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using WebApi.Helpers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,14 +14,38 @@ namespace WebApi.Controllers
     {
         
         [HttpGet("recognize-sentiment")]
-        public ActionResult<SentimentAnalysis.ModelInput> GetSentiment([FromQuery]SentimentAnalysis.ModelInput input)
+        public ActionResult<SentimentAnalysis.ModelOutput> GetSentiment([FromQuery]SentimentAnalysis.ModelInput input)
         {
             return Ok(SentimentAnalysis.Predict(input));
         }
+
         [HttpGet("recognize-image")]
-        public ActionResult<SentimentAnalysis.ModelInput> GetPicture([FromQuery] RecognizeNaturalImages.ModelInput input)
+        public IActionResult GetPicture(string imageInBase64)
         {
+            byte[] arrayOfBytes = Convert.FromBase64String(imageInBase64);
+            /*
+             
+        Console.WriteLine("Byte Array is: " + String.Join(" ", bytes));
+ 
+        string str = Convert.ToBase64String(bytes);
+        Console.WriteLine("The String is: " + str);*/
+            RecognizeNaturalImages.ModelInput input = new RecognizeNaturalImages.ModelInput()
+            {
+                ImageSource = arrayOfBytes
+            };
             return Ok(RecognizeNaturalImages.Predict(input));
+        }
+
+        private void UploadFiles(InputFileChangeEventArgs e)
+        {
+            var picture = e.GetMultipleFiles().FirstOrDefault();
+            var arrayOfBytes = new byte[picture.Size];
+            using (var ms = picture.OpenReadStream())
+            {
+                var bytesRead = ms.Read(arrayOfBytes,0, arrayOfBytes.Length);
+            }
+            string str = Convert.ToBase64String(arrayOfBytes);
+
         }
     }
 }
